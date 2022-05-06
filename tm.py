0938af2ff4fffad0d654ff6aeb2ddfde59eca727
Benjamin Baird <benjaminrbaird@gmail.com>
2022-05-06 00:23:57 -0400

    added comments + readability

--- tm.py
+++ tm.py
@@ -1,8 +1,6 @@
 class State:
     def __init__(self, name) -> None:
         self.name = name
-        self.start = False
-        self.final = False
         self.transition_rules = dict()
 
     #maps a state to a given symbol
@@ -12,44 +10,40 @@ class State:
     def read_symbol(self, reading_symbol):
         return self.transition_rules[reading_symbol]
 
-    def set_final(self, isFinal):
-        self.final = isFinal
 
-    def set_start(self, isStart):
-        self.start = isStart
-
-    def is_final(self):
-        return self.final
-
-class _Tape_Segment:
+#Node for tape linked list
+class _Tape_Node:
     def __init__(self, val='_', next=None, prev=None):
         self.value = val
         self.next = next
         self.prev = prev
 
+
+#linked list implementation of tape
 class Tape:
     def __init__(self, initial_tape=[]):
         self.start = None
         self.curr = None
         if initial_tape:
-            self.start = _Tape_Segment(initial_tape[0])
+            self.start = _Tape_Node(initial_tape[0])
             self.curr = self.start
             if initial_tape[1:]:
                 for v in initial_tape[1:]:
-                    next = _Tape_Segment(v, None, self.curr)
+                    next = _Tape_Node(val=v, next=None, prev=self.curr)
                     self.curr.next = next
                     self.curr = next
                 self.curr = self.start
 
+    #write value to current location on tape and move left or right
     def write(self, direction, write_val):
         self.curr.value = write_val
         if direction == 'R':
             if not self.curr.next:
-                self.curr.next = _Tape_Segment('_', None, self.curr)
+                self.curr.next = _Tape_Node(val='_', next=None, prev=self.curr)
             self.curr = self.curr.next
-        else:
+        elif direction == 'L':
             if not self.curr.prev:
-                self.curr.prev =_Tape_Segment('_', self.curr, None)
+                self.curr.prev =_Tape_Node(val='_', next=self.curr, prev=None)
             self.curr = self.curr.prev
 
     def read(self):
@@ -63,8 +57,6 @@ class TM:
     #creates a new NFA as specified by input NFA file.
     def new(self, tm_file_name='tm.txt'):
         self.states = {}
-        self.alphabet = set()
-        self.tape_alphabet = set()
         self.start = None
         self.accept = None
         self.reject = None
@@ -77,15 +69,12 @@ class TM:
                 self.states[state_name] = State(state_name)
                 if state_name == 'accept': self.accept = self.states[state_name]
                 if state_name == 'reject': self.reject = self.states[state_name]
-            #read alphabet
-            letters = tm_file.readline().rstrip().split(',')
-            for l in letters: self.alphabet.add(l)
-            #tape alphabet
-            letters = tm_file.readline().rstrip().split(',')
-            for l in letters: self.tape_alphabet.add(l)
+            #ignore alphabet
+            tm_file.readline()
+            #ignore tape alphabet
+            tm_file.readline()
             #set start state
             start_state = tm_file.readline().rstrip()
-            self.states[start_state].set_start(True)
             self.start = self.states[start_state]
             #add rules
             line = tm_file.readline()
@@ -95,18 +84,18 @@ class TM:
                 line = tm_file.readline()
             tm_file.close()
 
-    #simulates TM
+    #simulate TM
     def run(self, input_file_name='input.txt', output_file_name='output.txt'):
         #clear output file
         with open(output_file_name, 'w') as out:
-                    out.truncate(0)
-                    out.close()
+            out.truncate(0)
+            out.close()
         #read input file, follow transition rules for each string in file and write result to output.
         with open(input_file_name) as input_file:
             with open(output_file_name, 'w') as out:
                 for string in input_file:
                     self.accepts = False
-                    #sim function
+                    #load input to tape
                     self.tape = Tape([c for c in string.rstrip()])
                     self.simulate(self.start)
                     if self.accepts:
@@ -117,15 +106,14 @@ class TM:
             input_file.close()
 
     def simulate(self, current):
+        #end simulation if accept or reject state reached
         if current == self.accept: 
             self.accepts = True
             return
-        elif current == self.reject:
+        elif current == self.reject or self.tape.read() not in current.transition_rules:
             self.rejects =True
             return
-        if self.tape.read() not in current.transition_rules:
-            self.rejects = True
-            return
+        #read current tape value and write / move accordingly
         next = current.read_symbol(self.tape.read())
         self.tape.write(next[2], next[0])
         self.simulate(next[1])
